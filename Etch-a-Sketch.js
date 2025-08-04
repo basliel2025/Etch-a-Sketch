b4f33585754613aee14e945d85be9589342b4f61
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-08-04 16:15:32 +0300

    Correct grid size calculation and added event listener to handle changed

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,13 +1,17 @@
 //const { createElement } = require("react");
   const container = document.querySelector('.container');
   const size = 16;
-  const containerWidth = 500;
+  const containerWidth = container.clientWidth;
 for(let row=0; row<size;row++){
     for(let column=0;column<size;column++){
         const cells = document.createElement('div');
-        cells.style.width= 'containerWidth/size px';
-        cells.style.height = 'containerWidth/size px';
-        cells.classList.add('cell');
+        cells.style.width= containerWidth/size + 'px';
+        cells.style.height = containerWidth/size +'px';
+        cells.addEventListener('mouseover' , function(e){
+            e.target.classList.add('color');
+        });
+        cells.classList.add('cells');
         container.appendChild(cells);
     }
 }
+
