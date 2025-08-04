54e1a36719636e3b0d17adcaed6eb26322fc6e2c
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-08-04 19:10:50 +0300

    Rainbow and darken modes added

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,13 +1,38 @@
 const container = document.querySelector('.container');
 const size = 20;
 const containerWidth = container.clientWidth;
+const changeButton = document.createElement('button');
+changeButton.textContent = 'ColorType: Normal';
+changeButton.id = 'colorChanger';
+document.body.insertBefore(changeButton, container.nextSibling);
+let colorType = 'current';
 for(let row=0; row<size;row++){
     for(let column=0;column<size;column++){
         const cells = document.createElement('div');
         cells.style.width= containerWidth/size + 'px';
         cells.style.height = containerWidth/size +'px';
         cells.addEventListener('mouseover' , function(e){
-            e.target.classList.add('color');
+             if(colorType === 'rainbow'){
+                const r= Math.floor(Math.random() * 256);
+                const g= Math.floor(Math.random() * 256);
+                const b= Math.floor(Math.random() * 256);
+                 e.target.style.backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+                e.target.classList.add('color');
+             }
+              else if(colorType === 'darken'){
+                if(!e.target.dataset.hoverCount){
+                    e.target.dataset.hoverCount=0;
+                }
+                if(e.target.dataset.hoverCount<10){
+                    e.target.dataset.hoverCount++;
+                    const opacity = e.target.dataset.hoverCount*0.1;
+                    e.target.style.backgroundColor= 'rgba(0, 0, 0, '+ opacity + ')';
+                    e.target.classList.add('color');
+                }
+            }
+            else{
+                e.target.classList.add('color');
+            }
         });
         cells.classList.add('cells');
         container.appendChild(cells);
@@ -17,21 +42,41 @@ const button = document.querySelector('#adjust');
 button.addEventListener('click', function(){
       function newgrid(size){
       container.innerHTML= " ";
-      const cellSize = containerWidth/size;
+      const cellSize = container.clientWidth/size;
       for(let row=0;row<size ;row++){
         for(let column =0; column< size; column++){
         const newcell = document.createElement('div');
         newcell.style.width= cellSize + 'px';
         newcell.style.height = cellSize +'px';
         newcell.addEventListener('mouseover' , function(e){
-            e.target.classList.add('color');
+            if(colorType === 'rainbow'){
+                const r= Math.floor(Math.random() * 256);
+                const g= Math.floor(Math.random() * 256);
+                const b= Math.floor(Math.random() * 256);
+                e.target.style.backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+                e.target.classList.add('color');
+            }
+            else if(colorType === 'darken'){
+                if(!e.target.dataset.hoverCount){
+                    e.target.dataset.hoverCount=0;
+                }
+                if(e.target.dataset.hoverCount<10){
+                    e.target.dataset.hoverCount++;
+                    const opacity = e.target.dataset.hoverCount*0.1;
+                    e.target.style.backgroundColor= 'rgba(0, 0, 0, '+ opacity + ')';
+                    e.target.classList.add('color');
+                }
+            }
+            else{
+                e.target.classList.add('color');
+            }
         });
         newcell.classList.add('cells');
         container.appendChild(newcell);
         }
       }
     }
-    const grid = prompt("Enter grid size" ,'', '');
+     const grid = prompt("Enter grid size" ,'', '');
     const newSize = parseInt(grid);
     if(grid === null){
         alert('invalid input');
@@ -46,4 +91,18 @@ button.addEventListener('click', function(){
         alert('number is out of scale');
     }
 })
-  
+    changeButton.addEventListener('click', function(){
+        if(colorType === 'current'){
+            colorType = 'rainbow';
+            changeButton.textContent = 'ColorType: Rainbow';
+        }
+        else if(colorType === 'rainbow'){
+            colorType = 'darken';
+            changeButton.textContent = 'ColorType: Darken';
+        }
+        else{
+            colorType= 'current';
+            changeButton.textContent = 'ColorType: Normal';
+        }
+    });
+   
