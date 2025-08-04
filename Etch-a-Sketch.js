1ce832544af9d4631e4adb9aea9591e787d53a16
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-08-04 17:48:24 +0300

    The js file with resizable grid logic

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,7 +1,6 @@
-//const { createElement } = require("react");
-  const container = document.querySelector('.container');
-  const size = 16;
-  const containerWidth = container.clientWidth;
+const container = document.querySelector('.container');
+const size = 16;
+const containerWidth = container.clientWidth;
 for(let row=0; row<size;row++){
     for(let column=0;column<size;column++){
         const cells = document.createElement('div');
@@ -14,4 +13,37 @@ for(let row=0; row<size;row++){
         container.appendChild(cells);
     }
 }
-
+const button = document.querySelector('#adjust');
+button.addEventListener('click', function(){
+      function newgrid(size){
+      container.innerHTML= " ";
+      const cellSize = containerWidth/size;
+      for(let row=0;row<size ;row++){
+        for(let column =0; column< size; column++){
+        const newcell = document.createElement('div');
+        newcell.style.width= cellSize + 'px';
+        newcell.style.height = cellSize +'px';
+        newcell.addEventListener('mouseover' , function(e){
+            e.target.classList.add('color');
+        });
+        newcell.classList.add('cells');
+        container.appendChild(newcell);
+        }
+      }
+    }
+    const grid = prompt("Enter grid size" ,'', '');
+    const newSize = parseInt(grid);
+    if(grid === null){
+        alert('invalid input');
+    }
+    else if( isNaN(newSize)){
+        alert('Invalid size');
+    }
+    else if(newSize >= 1 && newSize <=100){
+        newgrid(newSize);
+    }
+    else if(newSize <1 || newSize > 100){
+        alert('number is out of scale');
+    }
+})
+  
